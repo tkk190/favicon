808f433d21a862a12e9c4ac6966db8bc76e56e2c
tkk <t.kleylein@web.de>
2024-08-04 20:33:27 +0200

    dynamic server

--- main.py
+++ main.py
@@ -1,52 +1,54 @@
+import shutil
+import os
+
 from PIL import Image, ImageFont, ImageDraw, ImageColor
 from favicons import Favicons
 
-def text_to_image(
-text: str,
-font_filepath: str,
-font_size: int,
-color: (int, int, int), #color is in RGB
-font_align="center"):
-    font = ImageFont.truetype(font_filepath, 620)
+def text_to_image(app_name: str, environment: str):
+    orange = (255, 153, 0)
+    blue = (0, 0, 100)
+    font = ImageFont.truetype('targa/Targa.ttf', 620)
     font2 = ImageFont.truetype('prosto/Prosto.ttf', 240)
     img = Image.new("RGBA", (1000, 1000), (255, 255, 255))
     draw = ImageDraw.Draw(img)
     draw.text(
         (950, 910),
-        text,
-        (0, 0, 100),
+        app_name,
+        blue,
         anchor="rs",
         font=font,
-        #align='center'
     )
     draw.text(
         (500, 30),
-        'MDCC',
-        color,
+        environment,
+        orange,
         anchor="mt",
         font=font2,
-        #align='center'
     )
-    draw.rectangle([(0, 0), (40, 1000)] , fill=color, outline=None)
-    draw.rectangle([(40, 960), (1000, 1000)] , fill=(0, 0, 100), outline=None)
-    # img.show()
-    # img.save("a_test.png")
+    draw.rectangle([(0, 0), (40, 1000)] , fill=orange)
+    draw.rectangle([(40, 960), (1000, 1000)] , fill=blue)
     return img
 
 if __name__ == '__main__':
-    res = text_to_image(
-        'MFV',
-        'targa/Targa.ttf',
-        600,
-        (255, 153, 0)
-    )
-    print(res)
-    res.show()
-    res.save("mfv.png")
+    app_name = 'MFV'
+
+    shutil.rmtree(app_name)
+    os.mkdir(app_name)
+    environments = ['MDCC', 'TEST', 'DEV', 'DEV 1', 'DEV 2', 'DEV 3', 'DEV 4', 'DEV 5', 'BUILD']
+    for environment in environments:
+        os.mkdir(f"{app_name}/{environment}")
+        os.mkdir(f"{app_name}/{environment}/favicon")
+        res = text_to_image(
+            app_name,
+            environment
+        )
+        print(res)
+#        res.show()
+        res.save(f"{app_name}/{environment}/{environment}.png")
 
 
-    YOUR_ICON = "mfv.png"
-    WEB_SERVER_ROOT = "favicon"
+        YOUR_ICON = f"{app_name}/{environment}/{environment}.png"
+        WEB_SERVER_ROOT = f"{app_name}/{environment}/favicon"
 
-    with Favicons(YOUR_ICON, WEB_SERVER_ROOT) as favicons:
-        favicons.generate()
+        with Favicons(YOUR_ICON, WEB_SERVER_ROOT) as favicons:
+            favicons.generate()
